6c63fe345c2c5effdc0dfe289b3273d2300761f6
Daniel Lagos <dlagos@formbay.com.au>
2018-03-01 08:21:39 +1100

    base32 table

--- src/hash.hs
+++ src/hash.hs
@@ -50,6 +50,7 @@ import System.Clock
 import Crypto.Hash.Algorithms
 import Crypto.MAC.HMAC
 import Data.Array
+import Data.Char
 
 --foo :: ByteArrayAccess a => a -> Int
 --foo s = Data.ByteArray.length s
@@ -77,43 +78,49 @@ main = do
 --    q = quot timestamp 30000
 
 
-table :: Array Integer Word8
-table = array [
-    (0, A),
-    (1, B),
-    (2, C),
-    (3, D),
-    (4, E),
-    (5, F),
-    (6, G),
-    (7, H),
-    (8, I),
-    (9, J),
-    (10, K),
-    (11, L),
-    (12, M),
-    (13, N),
-    (14, O),
-    (15, P),
-    (16, Q),
-    (17, R),
-    (18, S),
-    (19, T),
-    (20, U),
-    (21, V),
-    (22, W),
-    (23, X),
-    (24, Y),
-    (25, Z),
-    (26, 2),
-    (27, 3),
-    (28, 4),
-    (29, 5),
-    (30, 6),
-    (31, 7)]
+table :: Array Char Integer 
+table = array ("A", "Z") [
+    ("A", 0),
+    ("B", 1),
+    ("C", 2),
+    ("D", 3),
+    ("E", 4),
+    ("F", 5),
+    ("G", 6),
+    ("H", 7),
+    ("I", 8),
+    ("J", 9),
+    ("K", 10),
+    ("L", 11),
+    ("M", 12),
+    ("N", 13),
+    ("O", 14),
+    ("P", 15),
+    ("Q", 16),
+    ("R", 17),
+    ("S", 18),
+    ("T", 19),
+    ("U", 20),
+    ("V", 21),
+    ("W", 22),
+    ("X", 23),
+    ("Y", 24),
+    ("Z", 25)]
 
-base32decode :: String -> Maybe ByteString
-base32decode x = 
+--  (2, 26),
+--  (3, 27),
+--  (4, 28),
+--  (5, 29),
+--  (6, 30),
+--  (7, 31)]
+
+convert32 :: Char -> Integer
+convert32 a
+    | isUpper(a) = table ! a
+    | isDigit(a) = (digitToInt a) + 24
+
+-- base32decode :: String -> Maybe ByteString
+-- base32decode x = 
           
 authenticate :: ByteString -> ByteString
 authenticate secret = xxx (base32decode secret)
