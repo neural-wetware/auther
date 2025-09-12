a27c7e2b77279e1c290d606e2b25e0d890c3176f
Daniel Lagos <dlagos@formbay.com.au>
2025-09-12 14:27:18 +1000

    Move to flake style

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Haskell auther app using custom base32-bytestring, fully clean";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        ghc = pkgs.haskell.packages.ghc96;
+
+        auther = ghc.callCabal2nix "auther" ./. {};
+      in {
+        packages.default = auther;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            ghc.ghc
+            ghc.cabal-install
+          ];
+        };
+      });
+}
